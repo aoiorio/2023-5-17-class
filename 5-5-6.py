d76791bc7ba7320e40031c27592e8af2f60437ff
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-05-24 14:32:24 +0900

    update 5-5-6.py

--- 5-5-6.py
+++ 5-5-6.py
@@ -1,16 +1,26 @@
-broke_out = False
+# broke_out = False
+# seq = [1,2,3,4,5]
+
+# for x in seq:
+#     print(x)
+#     if x == 6:
+#         broke_out = True
+#         break
+
+# if not broke_out:
+#     print("breakでは抜けなかった")
+
+
+
 seq = [1,2,3,4,5]
+
 for x in seq:
     print(x)
-    if condition(x):
-        broke_out = True
+    if x == 6:
         break
+else:
+    print("breakでは抜けなかった")
 
-    
 
-
-    print("fixしました。")
-
-    print(x)
 
 
