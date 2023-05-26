d9f4d62683a25dfbd90c416dedb7f0f0dd5258b6
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-05-26 12:25:31 +0900

    update 6-4.py

---
+++ chapter06/6-4.py
@@ -0,0 +1,36 @@
+# def inc(x):
+#     return x + 1
+
+# foo = 10
+# foo += 1
+# foo = inc(foo)
+# print(foo)
+
+# キーワード引数
+# def hello_1(greeting='おはよう', name='折尾'):
+#     print(greeting, name)
+
+# hello_1('こんにちは',  '折尾')
+# hello_1(name='折尾', greeting='こんばんは')
+# hello_1()
+
+# 可変長引数
+def hello_2(title, param3, *params):
+    print(title, param3, params)
+
+
+
+
+
+# def hello_2(title, *params, param3):
+#     print(title, *params, param3)
+
+
+def hello_3(title, *params, param3):
+    print(title, *params, param3)
+# エラーになる呼び出し
+# hello_2('こんにちは', 'おはよう', 'さようなら')
+
+# 良い例
+hello_2("こんにちは", "おはよう>", "さようなら")
+hello_3('こんにちは', 'おはよう', param3='hwo are you doing?')
