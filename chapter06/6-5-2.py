c86bf081288dd5409c73c1e4121ddad7b4e69083
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-05-26 12:26:12 +0900

    6-5-2.py

---
+++ chapter06/6-5-2.py
@@ -0,0 +1,16 @@
+
+# def print_params(x, y, z=3, *pospar, **keypar):
+#     print(x, y, z)
+#     print(pospar)
+#     print(keypar)
+# print_params(1, 2, 3, 5, 6, 7, foo=1, bar=2)
+# print('-' * 10)
+# print_params(1, 2)
+
+# 逆の処理
+# def add(x, y, z, a):
+#     return x + y
+
+# params = (1, 2, 3)
+# print( add(1, *params) )
+
