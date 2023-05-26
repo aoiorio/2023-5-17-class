4d5f208a6853b5fe21b4e8112ddd8c39e136f773
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-05-26 12:24:45 +0900

    update func2.py

---
+++ chapter06/func2.py
@@ -0,0 +1,18 @@
+def fivePrint(arg):
+    for i in range(5):
+        print(arg)
+
+def fifthPrint(arg):
+    print(arg)
+    print(arg)
+    print(arg)
+    print(arg)
+    print(arg)
+name = input("What's your name?")
+fivePrint(name)
+
+age = input("How old are you?")
+fivePrint(age)
+
+birth = input("When is your birthday?")
+fivePrint(birth)
