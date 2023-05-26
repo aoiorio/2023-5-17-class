5540db60c361faacdf89b4b2e80adfa226d49125
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-05-26 12:28:28 +0900

    update 6.1.py

---
+++ chapter06/6.1.py
@@ -0,0 +1,21 @@
+
+def hello(name):
+    print('どうもこんにちは, ' + name)
+
+    
+    
+# def test():
+#     print("これは出力される")
+#     return 
+#     print("これは出力されない")
+
+def test2(x):
+    return
+
+
+# hello('折尾')
+# hello('山田')
+# test()
+y = test2(1)
+print(y)
+print("x")
