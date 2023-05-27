ba43f34769dc4cbd30c2636f9a698f729a7471dc
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-05-27 10:19:06 +0900

    add a file

---
+++ chapter05/5-5-7.py
@@ -0,0 +1,15 @@
+name = input("名前を入力してください：")
+
+if name == "葵":
+    print("太郎様ですね！お待ちしておりました！さあさあ、こちらのVIPルームへお入りください。")
+elif name == "葵":
+    pass
+    # あとで追加
+elif name == "折尾":
+    pass 
+    # print("花子どうも")
+    # print("花子")
+elif name == "悪代官だよね":
+    print("悪代官様ですね？")
+elif name == "悪代官のような何か":
+    print("アクセスは拒否されました")
