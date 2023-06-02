6cb8c9e95014789cef021d25a1c9d5afdb272097
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-02 12:42:18 +0900

    update 6-4.py

--- chapter06/6-4.py
+++ chapter06/6-4.py
@@ -1,16 +1,18 @@
-# def inc(x):
-#     return x + 1
+def inc(x):
+    return x + 1
 
-# foo = 10
-# foo += 1
-# foo = inc(foo)
-# print(foo)
+foo = 10
+foo += 1
+foo = inc(foo)
+print(foo)
 
 # キーワード引数
 # def hello_1(greeting='おはよう', name='折尾'):
 #     print(greeting, name)
 
-# hello_1('こんにちは',  '折尾')
+# hello_1('折尾', 'こんにちは')
+
+# キーワード引数
 # hello_1(name='折尾', greeting='こんばんは')
 # hello_1()
 
@@ -26,11 +28,10 @@ def hello_2(title, param3, *params):
 #     print(title, *params, param3)
 
 
-def hello_3(title, *params, param3):
-    print(title, *params, param3)
+# def hello_3(title, *params, param3):
+#     print(title, *params, param3)
 # エラーになる呼び出し
 # hello_2('こんにちは', 'おはよう', 'さようなら')
-
 # 良い例
-hello_2("こんにちは", "おはよう>", "さようなら")
-hello_3('こんにちは', 'おはよう', param3='hwo are you doing?')
+hello_2('こんにちは', 'おはよう', 'さようなら', '売買')
+# hello_3('こんにちは', 'おはよう', '売買', param3='hwo are you doing?')
