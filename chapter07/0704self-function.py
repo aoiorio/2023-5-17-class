a733d4339c0bcbf8e5b79b0ed675ed95ac1ab80f
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-02 12:40:10 +0900

    update 0704self-function.py

---
+++ chapter07/0704self-function.py
@@ -0,0 +1,13 @@
+class Class1:
+    def method1(self):
+        print(f'引数selfの値(method1): {self}')
+def function1(self):
+    print(f'引数selfの値(function1): {self}')
+
+# 実行
+instance1 = Class1()
+instance1.method1()
+instance1.method1() == function1(self="aa")
+function1()
+# instance1.method1 = function1
+# instance1.method1()
