c62aabd3156375f99401479795bc5244097379a5
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-04 16:27:15 +0900

    update main.py

--- chapter07/main.py
+++ chapter07/main.py
@@ -1,23 +1,26 @@
 class Pokemon:
-    def __init__(self, name, hp, type):
+    def __init__(self, name, hp, type, host):
         self.name = name
         self.hp = hp
         self.type = type
+        self.host = host
     def getName(self):
         return self.name
     def getHp(self):
         return self.hp
     def getType(self):
         return self.type
+    def getHost(self):
+        return self.host
     def damage(self, damage):
         self.hp -= damage
     def printInfo(self):
-        print(f"{self.name}は、{self.type}タイプ。HPは{self.hp}です。")
+        print(f"{self.name}は、{self.type}タイプ。HPは{self.hp}です。飼い主は、{self.host}です。")
     
 
         
-pikatyu = Pokemon("ピカチュウ", 100, "でんき")
-hitokage = Pokemon("ヒトカゲ", 50, "ひ")
+pikatyu = Pokemon("ピカチュウ", 100, "でんき", "たかし")
+hitokage = Pokemon("ヒトカゲ", 50, "ひ", "誰か")
 pikatyu.printInfo() #ピカチュウのタイプやHPを表示してくれる
 hitokage.printInfo() # ヒトカゲのタイプやHPを表示してくれる
     
