29a679427ed0300fe947dc381c396a0b5d71dc2e
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-04 16:34:02 +0900

    update 0703person.py

--- chapter07/0703person.py
+++ chapter07/0703person.py
@@ -1,6 +1,8 @@
 from mod import Person
 
 #  インスタンスの生成
+meee = Person()
+
 foo = Person()
 print(foo) # person objectになってperson class の関数が使えるようになる
 bar = Person()
@@ -8,3 +10,4 @@ foo.set_name('Justin Bieber')
 bar.set_name('Lii Nas X')
 foo.greet()
 bar.greet()
+meee.greet()
