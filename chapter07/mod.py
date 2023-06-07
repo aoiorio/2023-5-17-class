e0a3d6397e197258e7ece8a6400d65860acd27ed
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-07 17:29:52 +0900

    update mod.py

---
+++ chapter07/mod.py
@@ -0,0 +1,7 @@
+class Person:
+    def set_name(self, name):
+        self.name=name
+    def get_name(self):
+        return self.name
+    def greet(self):
+        print(f"こんにちは。私は{self.name}です！")
