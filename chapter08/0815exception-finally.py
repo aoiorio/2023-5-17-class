4e8772e8fd6bf8141f5ed676b7dca3bec4746915
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-09 12:35:59 +0900

    update 0815exception-finally.py

---
+++ chapter08/0815exception-finally.py
@@ -0,0 +1,13 @@
+x = None
+try:
+    # ファイルを開く
+    x = 1 / 1
+except Exception as e:
+    print("不正な入力です：", e)
+    print("処理を継続します。")
+finally: # 必ずtry の中が実行し終わったら、finally の中にあるやつを実行する
+    # ファイルを閉じる
+    print("後始末中・・・")
+    del x
+    
+print("さらに処理を継続します。")
