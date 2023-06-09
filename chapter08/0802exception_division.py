e3beb40007ead827251dd97a28c2ffba8e9732a8
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-09 12:33:50 +0900

    update 0802exception_division.py

---
+++ chapter08/0802exception_division.py
@@ -0,0 +1,15 @@
+try:
+    x = int(input("最初の数を入力してください。"))
+    y = int(input("二番目の数を入力してください")) 
+    print(x/y)
+
+except (ZeroDivisionError, ValueError, NameError) as e:
+    print(e)
+# except (ZeroDivisionError, ValueError):
+#     print("値が不正です")
+
+
+    # print("二番目の数に0以外を入力してください！")
+# except ValueError:
+#     print("数値を入力してください。", n)
+print("=====END=====")
