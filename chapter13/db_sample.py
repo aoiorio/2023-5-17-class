20a0c0d6bc9d988e68c39fccb021652b1f5534ad
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-30 12:27:33 +0900

    added db_sample.py

---
+++ chapter13/db_sample.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+#データベースに接続
+conn = sqlite3.connect('text.db')
+# データベースのカーソルを取得
+cursor = conn.cursor()
+
+# テーブルを作成 executeは実行という意味
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS test (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL
+);
+''')
+
+query ='INSERT INTO test (name, email) VALUES (?, ?)'
+cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
+cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
+cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
+cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
+
+conn.commit()
+conn.close()
