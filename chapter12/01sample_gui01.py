c183b6f53658a34d3e6054889e35438751d15aed
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-06-30 12:25:34 +0900

    01sample_gui01.py

---
+++ chapter12/01sample_gui01.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+def click():
+    print("You clicked it!")
+
+top = Tk() # インスタンス化 topという変数にtkinterのTk()を代入これがオブジェクト？？
+
+top.geometry("400x300")
+btn = Button(text="click", command=click).pack() # 簡潔にpackができる
+# btn.pack() # ボタンをウィンドウに表示するよっていう処理　ボタンを作ったらpackを実行させてあげてください
+
+hello_label = Label(text="Hello").pack()
+# second = Toplevel()
+# second_label = Label(second, text="Second").pack() # 違うウィンドウで表示される
+top.mainloop()
