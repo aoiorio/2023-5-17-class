ec6170bdf7336756b597cbcf5bab6d6a766f87fc
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-07-03 20:22:47 +0900

    Update db_sample.py

--- chapter13/db_sample.py
+++ chapter13/db_sample.py
@@ -13,7 +13,7 @@ cursor.execute('''
         email TEXT NOT NULL
 );
 ''') 
-# sqlite3の書き方は全て英語の大文字です。
+# sqlite3の書き方は全て英語の大文字です。　testというテーブルがなかったら新たに作るというコード
 
 query ='INSERT INTO test (name, email) VALUES (?, ?)'
 cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
