d9236d4f4c9a3b954b5796b8772e0fa73391eaf3
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-05 17:36:32 +0900

    added db_user.py

---
+++ chapter13/db_user.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+#データベースに接続
+conn = sqlite3.connect('user.db')
+# データベースのカーソルを取得
+cursor = conn.cursor()
+
+# テーブルを作成 executeは実行という意味
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        password TEXT NOT NULL,
+        address TEXT NOT NULL,
+        age INTEGER NOT NULL 
+);
+''')
+# NOT NULL はあってもなくてもいい
+query ='INSERT INTO user (name, password, address, age) VALUES (?, ?, ?, ?)' # ??は後で入れるよという意味
+cursor.execute(query, ('Aoi Orio', 'pass101', 'okinawa', 15)) # ?, ? に入れたい値を入れることができる
+cursor.execute(query, ('Uemura', 'houhou1', 'tokyo', 26))
+cursor.execute(query, ('DAreka', 'muo', 'hawaii', 89))
+cursor.execute(query, ('hai', 'wai', 'america', 35))
+
+conn.commit()
+conn.close()
