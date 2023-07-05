e53f644fdbfe2ddc37860ec04abb2fdf9f0e4bad
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-05 17:23:50 +0900

    added db_sample.py

--- chapter13/db_sample.py
+++ chapter13/db_sample.py
@@ -1,26 +1,26 @@
 import sqlite3 # sqlite3を追加
 
 #データベースに接続
-conn = sqlite3.connect('text.db')
+conn = sqlite3.connect('user.db')
 # データベースのカーソルを取得
 cursor = conn.cursor()
 
 # テーブルを作成 executeは実行という意味
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS test (
+    CREATE TABLE IF NOT EXISTS user (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        email TEXT NOT NULL
+        password TEXT NOT NULL,
+        address TEXT NOT NULL,
+        age INTEGER NOT NULL
 );
 ''') 
 # testというテーブルがなかったら新たに作るというコード
 # sqlite3の書き方は全て英語の大文字です。　
 
-query ='INSERT INTO test (name, email) VALUES (?, ?)'
-cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
-cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
-cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
-cursor.execute(query, ('Yamada Taro', 'taro@email.com')) # ?, ? に入れたい値を入れることができる
+query ='INSERT INTO user (name, password, address, age) VALUES (?, ?, ?, ?)' # ??は後で入れるよという意味
+cursor.execute(query, ('Aoi Orio', 'pass101', 'okinawa', 15))# ?, ? に入れたい値を入れることができる
+
 
 conn.commit()
 conn.close()
