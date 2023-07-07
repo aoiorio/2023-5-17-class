64a0d95102a57dfe00e3b3444233e2a6b5672c4a
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-07 18:58:20 +0900

    added goods.py

---
+++ chapter13/goods.py
@@ -0,0 +1,11 @@
+from connection import *
+
+
+class Goods:
+    def __init__(self):
+        self.connection = SQLiteConnection('test.db')
+        self.connection.connect()
+    def goods_insert(self, goods_name, goods_no):
+        query = 'INSERT INTO goods (goods_name, goods_no) VALUES (?, ?)'
+        self.connection.execute_query(query, goods_name, goods_no)
+        self.connection.close()
