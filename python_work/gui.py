0891f6c39cefc5b3362091b5f43129e509e58c68
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-07-08 19:40:30 +0900

    Update gui.py

--- python_work/gui.py
+++ python_work/gui.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 
 layout = [[sg.Text("Hello from PySimpleGUI")], 
         [sg.Text("GoodMorning from PySimpleGUI")], 
-        [sg.Text("GoodMorning from PySimpleGUI"), sg.InputText()],]
+        [sg.Text("GoodMorning from PySimpleGUI"), sg.InputText()],] 
 
 window = sg.Window('title', layout)
 evnet, values = window.read()
