141aa1c89034e77acadda68f7ec224f4c87c3bc7
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-07-08 19:41:27 +0900

    Update gui.py

--- python_work/gui.py
+++ python_work/gui.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 
 layout = [[sg.Text("Hello from PySimpleGUI")], 
         [sg.Text("GoodMorning from PySimpleGUI")], 
-        [sg.Text("GoodMorning from PySimpleGUI"), sg.InputText()],] # テキストを入力することがで
+        [sg.Text("GoodMorning from PySimpleGUI"), sg.InputText()],] # テキストを入力することができ
 
 window = sg.Window('title', layout)
 evnet, values = window.read()
