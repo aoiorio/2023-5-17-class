b2909a35c32798efc024807eb1d69e49943961ba
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-12 16:47:16 +0900

    added python_work1/gui.py

---
+++ python_work1/gui.py
@@ -0,0 +1,11 @@
+import PySimpleGUI as sg
+# PySimpleGUIをimport(追加)
+
+
+layout = [[sg.Text("Hello from PySimpleGUI")], 
+        [sg.Text("GoodMorning from PySimpleGUI")], 
+        [sg.Text("GoodMorning from PySimpleGUI"), sg.InputText()],] # テキストを入力することが出来る
+
+window = sg.Window('title', layout)
+evnet, values = window.read()
+window.close()
