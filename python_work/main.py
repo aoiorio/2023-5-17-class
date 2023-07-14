75fa3d68ca3d7935acf3201b459edb5775b8eb5d
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-14 11:32:12 +0900

    update goods.db and main.py

--- python_work/main.py
+++ python_work/main.py
@@ -7,7 +7,7 @@ layout = [
     [sg.Text("1~7の好きな数字を入力してください"), sg.Text(size=(12, 1), key="-OUTPUT-")],
     [sg.Input(key="-IN-")],
     [sg.Button("Show")],
-    
+
 ### ここから追加
     [sg.Text("追加データを入力してください"), sg.Text(size=(12, 1), key="-OUTPUT-")],
     [sg.Text("商品"),sg.Input(key="-NAME-"),sg.Text("商品コード"),sg.Input(key="-NO-"),],
@@ -18,6 +18,7 @@ layout = [
 
 window = sg.Window("Window Title", layout)
 
+# while で処理を書いている。
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit":
@@ -31,7 +32,7 @@ while True:
     if event == "Regist":
         goods = Goods()
         goods.goods_insert(values["-NAME-"], values["-NO-"])
-        
+
         window["-COMPLETE-"].update(values["-NAME-"] + 'が追加されました')
-### ここまで        
+### ここまで
 window.close()
