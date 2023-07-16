6b82a25e60b32a20d63044b13578683c0cca9dc7
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-07-16 16:10:16 +0900

    Update goods.py

--- python_work/goods.py
+++ python_work/goods.py
@@ -2,7 +2,7 @@ from connection import *
 
 
 class Goods:
-    def __init__(self):
+    def __init__(self): #
         self.connection = SQLiteConnection("goods.db")
         self.connection.connect()
 
