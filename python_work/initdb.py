19aa2a853f78a2c0c32059697825c8e9bb673382
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-07-18 18:33:44 +0900

    Update initdb.py

--- python_work/initdb.py
+++ python_work/initdb.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("goods.db") # goods.dbのデータベースに繋げて
+conn = sqlite3.connect("goods.db") # goods.dbのデータベースに繋げている
 cursor = conn.cursor()
 cursor.execute(
     """
