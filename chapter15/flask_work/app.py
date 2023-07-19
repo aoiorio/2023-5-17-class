fdec85829818e0b7bdcc63935e83d77d72fe8594
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-19 15:30:14 +0900

    added app.py

--- chapter15/flask_work/app.py
+++ chapter15/flask_work/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def index():
     return render_template('html/hello.html') # html/index.htmlを表示しますよという指定
 
