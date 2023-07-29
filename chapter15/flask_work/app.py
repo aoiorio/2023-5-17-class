98ca6f84b9112e7ea5d066e8520b239058134edf
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-07-29 19:09:49 +0900

    Update app.py

--- chapter15/flask_work/app.py
+++ chapter15/flask_work/app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/flask')
 def flask_hello():
-    return render_template('html/flask.html') 
+    return render_template('html/flask.html') #
 
 # @app.route('/python')
 # def hello_python():
