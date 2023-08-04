97f10ec58c791401e4187688500824eb4bdad0a4
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-08-04 21:27:14 +0900

    Update app.py

--- chapter15/flask_work/app.py
+++ chapter15/flask_work/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('html/hello.html') # html/index.htmlを表示しますよという指定
 
-@app.route('/flask') #/flaskというpathを
+@app.route('/flask') #/flaskというpathを設定したら
 def flask_hello():
     return render_template('html/flask.html') # html/flask.htmlファイルに飛ばす
 
