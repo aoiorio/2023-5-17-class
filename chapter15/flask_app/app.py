9d1f42fa69cf762c3bf397196fcd38248964e5b9
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-08-05 19:00:57 +0900

    Update app.py

--- chapter15/flask_app/app.py
+++ chapter15/flask_app/app.py
@@ -25,7 +25,7 @@ def index():
     for task in tasks:
         print(task.title, task.detail)
 
-    return render_template('index.html', tasks=tasks)
+    return render_template('index.html', tasks=tasks) 
 
 @app.route('/user_form')
 def user_page():
