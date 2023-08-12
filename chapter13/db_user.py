d85d5825af0a6847f9eb7fffd0a055a715215d2d
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-08-12 11:51:21 +0900

    Update db_user.py

--- chapter13/db_user.py
+++ chapter13/db_user.py
@@ -24,4 +24,4 @@ cursor.execute(query, ('DAreka', 'muo', 'hawaii', 89))
 cursor.execute(query, ('hai', 'wai', 'america', 35))
 
 conn.commit() # 追加した内容を確約する
-conn.close() # databaseを閉じる
+conn.close() # databaseを閉じるclose
