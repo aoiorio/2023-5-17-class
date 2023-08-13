9dfd5d7f136bb79bb9bc0fe79d3955c027985a56
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-08-13 13:14:52 +0900

    Update goods.py

--- chapter13/goods.py
+++ chapter13/goods.py
@@ -3,7 +3,7 @@ from connection import *
 
 class Goods:
     def __init__(self):
-        self.connection = SQLiteConnection('test.db')
+        self.connection = SQLiteConnection('test.db') #
         self.connection.connect()
     def goods_insert(self, goods_name, goods_no):
         query = 'INSERT INTO goods (goods_name, goods_no) VALUES (?, ?)'
