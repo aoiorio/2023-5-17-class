8def7968ab2ab1af35f0da42848f0e0efa4bc8e8
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-08-20 17:25:02 +0800

    Update main.py

--- python_work/main.py
+++ python_work/main.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-from goods import * # *は全てという意味（In this case goodsの全てを（クラスなど）をim）
+from goods import * # *は全てという意味（In this case goodsの全てを（クラスなど）をimport）
 
 sg.theme("Dark Blue 3")
 
