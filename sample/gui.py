56579a8ef9224448af38f0b76848424ea2ed6a37
aoi:orio <130330538+aoiorio@users.noreply.github.com>
2023-08-21 11:31:09 +0800

    Update gui.py

--- sample/gui.py
+++ sample/gui.py
@@ -20,7 +20,7 @@ while True:  # Event Loop
     if event == "Show":
         # change the "output" element to be the value of "input" element
         window["-OUTPUT-"].update(values["-IN-"])
-    if event == "Register": # もしeventがRegisterに
+    if event == "Register": # もしeventがRegisterになった
         window["-update-"].update(f'{values["-THING-"]}が追加されました。') # Keyを設定したらそのKeyに対してアクションを起こすことができる
 
 window.close() #ファイルを閉じる動作
