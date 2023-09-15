d5a007c5a22c7497a9e684a8261c1ee8af338bc4
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-15 10:47:49 +0900

    added comments

--- web_api/weather_api/main.py
+++ web_api/weather_api/main.py
@@ -2,8 +2,10 @@ import requests
 
 
 def search_weather(self):
+    # APIの取得
     url = f"https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m"
     response = requests.get(url)
+    # 辞書型になっている値をjsonで取得してdataに代入する
     data = response.json()
     print(data)
 
