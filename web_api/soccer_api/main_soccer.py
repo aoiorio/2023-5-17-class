bd087b97d980bfd6f490eb694d1b8acc3ba42873
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-17 01:17:19 +0900

    deleted comment out

--- web_api/soccer_api/main_soccer.py
+++ web_api/soccer_api/main_soccer.py
@@ -16,15 +16,3 @@ def soccer_api():
 
 soccer_api()
 # 872b5ca0a4f64cf281154f6f53d38962
-
-# print("-----------------------------------------------")
-
-# import http.client
-# import json
-
-# connection = http.client.HTTPConnection('api.football-data.org')
-# headers = { 'X-Auth-Token': '872b5ca0a4f64cf281154f6f53d38962' }
-# connection.request('GET', '/v2/competitions/DED', None, headers )
-# response = json.loads(connection.getresponse().read().decode())
-
-# print (response)
