0ff6bae5528d6ff8fa7f55d758b4109090c41b5f
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-21 00:50:19 +0900

    deleted some space

--- python_work1/gui.py
+++ python_work1/gui.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg # PySimpleGUIをimport(追加)
 
 
-layout = [[sg.Text("Hello from PySimpleGUI")], 
-        [sg.Text("GoodMorning from PySimpleGUI")], 
+layout = [[sg.Text("Hello from PySimpleGUI")],
+        [sg.Text("GoodMorning from PySimpleGUI")],
         [sg.Text("GoodMorning from PySimpleGUI"), sg.InputText()],] # テキストを入力することが出来る
 
 window = sg.Window('title', layout)
