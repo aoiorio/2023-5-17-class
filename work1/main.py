487fcc3bb79c11d5ea2b3f812ae926b07e268110
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-29 00:53:48 +0900

    update main.py

--- work1/main.py
+++ work1/main.py
@@ -20,11 +20,11 @@ jobs = set()
 for job in soup.body.ul('li'):
     jobs.add(f'{job.a.string})')
 print(jobs)
-# # print()
-# # h2 tagのものを取得する
+# print()
+# h2 tagのものを取得する
 # for job in soup.body.section('h2'):
 #     jobs.add(f'{job.a.string}) ({job.a["href"]}')
 
 # str.lowerでabc順に表示しますよーというオプションです
 # print('\n'.join(sorted(jobs, key=str.lower)))
-# # print(web_site_html_text)
+# print(web_site_html_text)
