e985ce4bc44a19090bce8e8ed498de828130f2ff
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-03-08 19:32:06 +0800

    🍕 add: comment

--- web_api/post_search_sample/main.py
+++ web_api/post_search_sample/main.py
@@ -25,6 +25,7 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
+# get weather information from open metro API
 def search_weather(self):
     url = f'https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m'
     response = requests.get(url)
